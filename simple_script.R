961e395d6561dc468178b34a04e6fc55f2e4bc93
Plamena P. Powla <97992961+Plamena40@users.noreply.github.com>
2024-08-09 18:53:30 -0400

    Update simple_script.R

--- simple_script.R
+++ simple_script.R
@@ -1,6 +1,5 @@
 #################################################
-#    An XGB model with tuning grid of size 3840 and sample size of 1000
-#    binary outcome variable and two randomly generated predictors.
+#    A simple XGB model with two randomly generated predictors and a binary outcome.
 #    ROC curve is printed as the result.
 #    Plamena P. Powla
 ##################################################
@@ -15,27 +14,25 @@ library(ROCR)
 library(xgboost)
 
 
-df <- data.frame(id=1:1000)
-df$Age <- rnorm(1000, 50, 10)
-
-df$Mortality <- sample(LETTERS[1:2], 1000, replace=TRUE, prob=c(.5,.5))
-
-df$Injury <- sample(LETTERS[1:4], 1000, replace=TRUE, prob=c(.15,.25, .4, .2))
+df <- data.frame(id=1:100)
+df$A <- rnorm(100, 50, 10)
+df$B <- sample(LETTERS[1:4], 100, replace=TRUE, prob=c(.15,.25, .4, .2))
+df$C <- sample(LETTERS[1:2], 100, replace=TRUE, prob=c(.5,.5))
 
 
 ctrl <- trainControl(method = "LOOCV",
                      classProbs = TRUE, summaryFunction = twoClassSummary,
                      verboseIter = T, savePredictions = T, returnResamp = "final")
 
-xgbGrid <- expand.grid(nrounds = c(15:30), 
-                       max_depth = 2:6,
-                       eta = c(0,.1,.2,.3),
+xgbGrid <- expand.grid(nrounds = c(20:30), 
+                       max_depth = 2:4,
+                       eta = c(0,.1,.2),
                        colsample_bytree = 1,
                        min_child_weight = c(0,.1),
-                       subsample = c(.8,.9,1),
+                       subsample = c(.9,1),
                        gamma = c(0, .1))
 
-xgb_mod <- train(Mortality ~ Age + Injury, 
+xgb_mod <- train(C ~ A + B, 
                  data = df, method = "xgbTree", metric = "ROC",
                  trControl = ctrl, tuneGrid = xgbGrid, 
                  preProcess = c("center","scale"))
