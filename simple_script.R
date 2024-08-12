d9ba7d91a47e85d89ee9b80cdec04d60704bd2ac
Plamena P. Powla <97992961+Plamena40@users.noreply.github.com>
2024-08-12 18:05:53 -0400

    Update simple_script.R

--- simple_script.R
+++ simple_script.R
@@ -15,15 +15,15 @@ df <- data.frame(id=1:100000)
 b0 <- -10
 b1 <- 1.5
 b2 <- 2
-df$A <- rnorm(100000,0,3)
+df$A <- rnorm(1000,0,3)
 df$B <- df$A^2
-df$e <- rnorm(100000,1,10)
+df$e <- rnorm(1000,1,10)
 df$p <- exp(b0+b1*df$A+b2*df$B+df$e)/(1+exp(b0+b1*df$A+b2*df$B+df$e))
-df$C <- rbinom(100000,1, prob = df$p)
+df$C <- rbinom(1000,1, prob = df$p)
 df$C <- ifelse(df$C == 1, "A", "B")
 
 
-ctrl <- trainControl(method = "LOOCV",
+ctrl <- trainControl(method = "CV", number = 5,
                      classProbs = TRUE, summaryFunction = twoClassSummary,
                      verboseIter = T, savePredictions = T, returnResamp = "final")
 
